53d9fab8eabadc7825a46451f5d5fa2d4df04ee8
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-19 12:45:22 +0200

    Remove Class1 and add Game and TournamentDetails classes
    This commit removes the `Class1` class from the project.
    It introduces two new classes: `Game`, which contains properties for
    game details, and `TournamentDetails`, which includes properties for
    tournament information and a collection of games.

---
+++ Tournament.Core/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace Tournament.Core;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Time { get; set; }
+    public int TtournamentId { get; set; }
+}
