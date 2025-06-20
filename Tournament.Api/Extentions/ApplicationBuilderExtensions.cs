c8c9c9f391f0314058c20c3207572f5835fe48ff
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-20 19:20:28 +0200

    Implement ApplicationBuilderExtensions

---
+++ Tournament.Api/Extentions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tournament.Data.Data;
+
+namespace Tournament.Api.Extentions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static async Task SeedDataAsync(this IApplicationBuilder builder)
+    {
+        using (var scope = builder.ApplicationServices.CreateScope())
+        {
+            var serviceProvider = scope.ServiceProvider;
+            var tournamentContext = serviceProvider.GetRequiredService<TournamentApiContext>();
+            await tournamentContext.Database.MigrateAsync();
+            if (await tournamentContext.TournamentDetails.AnyAsync()) 
+            {
+                return;
+            }
+            try
+            {
+                // var tournaments = SeedData(10);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
