99a479934222294e57579e682deb295af105433b
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-23 16:44:55 +0200

    feat(repository): implement IGameRepository and create GameRepository

---
+++ Tournament.Data/Data/Repositories/GameRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core;
+using Tournament.Core.Repositories;
+
+namespace Tournament.Data.Data.Repositories;
+
+public class GameRepository : IGameRepository
+{
+    void IGameRepository.Add(Game game)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<bool> IGameRepository.AnyAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<IEnumerable<Game>> IGameRepository.GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<Game> IGameRepository.GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IGameRepository.Remove(Game game)
+    {
+        throw new NotImplementedException();
+    }
+
+    void IGameRepository.Update(Game game)
+    {
+        throw new NotImplementedException();
+    }
+}
