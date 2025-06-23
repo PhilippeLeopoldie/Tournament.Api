cba5a361d99623215b1558f8f3388e95de88f395
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-23 17:45:41 +0200

    feat(UnitOfWork): implement pattern for managing repository transactions

---
+++ Tournament.Data/Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Repositories;
+
+namespace Tournament.Data.Data.Repositories;
+
+public class UnitOfWork(
+    TournamentApiContext context,
+    ITournamentRepository tournamentRepository,
+    IGameRepository gameRepository
+    ) : IUnitOfWork
+{
+
+    public ITournamentRepository TournamentRepository => tournamentRepository;
+
+    public IGameRepository GameRepository => gameRepository;
+
+    
+
+    public async Task<int> CompleteAsync()
+    {
+        return await context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        context.Dispose();
+    }
+}
