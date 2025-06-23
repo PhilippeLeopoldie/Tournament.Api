39a0264105169238c4edf0c8f19823518b7382dd
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-23 16:29:51 +0200

    feat(repository): implement ITournamentRepository and create TournamentRepository

---
+++ Tournament.Data/Data/Repositories/TournamentRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core;
+using Tournament.Core.Repositories;
+
+namespace Tournament.Data.Data.Repositories;
+
+public class TournamentRepository(TournamentApiContext context) : ITournamentRepository
+{
+    void ITournamentRepository.Add(TournamentDetails tournamentDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<bool> ITournamentRepository.AnyAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<IEnumerable<TournamentDetails>> ITournamentRepository.GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<TournamentDetails> ITournamentRepository.GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ITournamentRepository.Remove(TournamentDetails tournamentDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ITournamentRepository.Update(TournamentDetails tournamentDetails)
+    {
+        throw new NotImplementedException();
+    }
+}
