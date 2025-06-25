3c7b1666dc518405c3502f82462cd52b50728ea1
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-25 11:55:16 +0200

    feat(Dto): implement GameDto and TournamentDto

---
+++ Tournament.Core/Dtos/GameDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Dto;
+
+public record GameDto
+{
+    public string Title { get; set; }
+    public DateTime Time { get; set; }
+}
