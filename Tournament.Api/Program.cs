1a5b4244470ffd25353e8c8bea9dcfd221f95298
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-25 11:56:20 +0200

    feat(mapping): implement mappings between DTOs and entities

--- Tournament.Api/Program.cs
+++ Tournament.Api/Program.cs
@@ -27,6 +27,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(typeof(TournamentMappings));
 
         var app = builder.Build();
         
---
+++ Tournament.Data/Data/TournamentMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Dto;
+using Tournament.Core.Entities;
+
+namespace Tournament.Data.Data;
+
+public class TournamentMappings : Profile
+{
+    public TournamentMappings() 
+    {
+        CreateMap<TournamentDetail, TournamentDto>();
+        CreateMap<Game, GameDto>();
+    }
+}
