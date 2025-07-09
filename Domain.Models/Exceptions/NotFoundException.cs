c2eb2b7cf305b1d8c4bf197b4b28edbbade3f79f
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-07-09 14:37:49 +0200

    Add NotFoundException and specific not found exceptions
    Introduce an abstract class `NotFoundException` with a `Title` property and a constructor for message initialization. Implement `TournamentNotFoundException` and `GameNotFoundException` classes that inherit from `NotFoundException`, providing constructors for formatted not found messages based on ID or title.

---
+++ Domain.Models/Exceptions/NotFoundException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Exceptions;
+
+public abstract class NotFoundException: Exception
+{
+    public string Title { get; set; }
+
+    protected NotFoundException(string message, string title = "Not found"): base(message)
+    {
+        Title = title;
+    }
+}
+
+public class TournamentNotFoundException : NotFoundException
+{
+    public TournamentNotFoundException(int id) : base($"The tournament with id: {id} is not found!")
+    {
+    }
+
+    public TournamentNotFoundException(string title) : base($"The tournament with title: {title} is not found!")
+    {
+    }
+
+}
+
+
+
+public class GameNotFoundException : NotFoundException
+{
+    public GameNotFoundException(int id) : base($"The tournament with id: {id} is not found!")
+    {
+    }
+
+    public GameNotFoundException(string title) : base($"The tournament with title: {title} is not found!")
+    {
+    }
+}
