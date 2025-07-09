6b5e99cb2e511cd80caf12394a4d96ebfb0a8056
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-07-09 14:57:22 +0200

    Add BadRequestException and BadRequest classes
    Introduce an abstract class `BadRequestException` for handling bad request errors, featuring a `Title` property and a constructor for message initialization. Implement a concrete `BadRequest` class with two constructors for specific error messages related to ID mismatches and missing patch documents.

---
+++ Domain.Models/Exceptions/BadRequestExeption.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Exceptions;
+
+public abstract class BadRequestException : Exception
+{
+    public string Title { get; set; }
+    protected BadRequestException(string message, string title = "Bad request") : base(message)
+    {
+        Title = title;
+    }
+}
+
+public class BadRequest : BadRequestException
+{
+    public BadRequest(int id) : base($"id: '{id}' do not match id from body")
+    {
+    }
+    public BadRequest() : base($"No patchDocument")
+    {
+    }
+}
