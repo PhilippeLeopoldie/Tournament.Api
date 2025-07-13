14ff05b7041c40a55cea0a26d05354c2fec7cbe7
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-07-13 15:12:04 +0200

    Add unit testing project for Tournament application
    - Introduced "Tournament.Tests" project in the solution.
    - Updated solution configuration to include the new test project.
    - Created `ControllerTournamentTest` class for testing `TournamentsController`.
    - Added `GetTournaments_ShouldReturnAllTournaments` test method.
    - Implemented helper methods for sample data in tests.
    - Configured `Tournament.Tests.csproj` for .NET 8.0 with necessary package references.
    - Established project references to main application projects for testing access.

---
+++ Tournament.Tests/ControllerTournamentTest.cs
@@ -0,0 +1,124 @@
+using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+using Domain.Contracts;
+using Domain.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Services.Contracts;
+using Tournament.Presentation.Controllers;
+using Tournaments.Shared.Dtos;
+using Tournaments.Shared.Request;
+
+namespace Tournament.Tests;
+
+public class ControllerTournamentTest
+{
+    private readonly Mock<IServiceManager> _mockServiceManager;
+    private readonly Mock<ITournamentService> _mockTournamentService;
+    private readonly TournamentsController _tournamentsController;
+    
+
+
+    public ControllerTournamentTest()
+    {
+        _mockServiceManager = new Mock<IServiceManager>();
+        _mockTournamentService = new Mock<ITournamentService>();
+
+        _mockServiceManager.Setup(x => x.TournamentService).Returns(_mockTournamentService.Object);
+
+        _tournamentsController = new TournamentsController(_mockServiceManager.Object);
+
+        // to create a basic HttpContext so that Response.Headers from controller is available during the test
+        _tournamentsController.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+    }
+
+    
+
+    [Fact]
+    public async Task GetTournaments_ShouldReturnAllTournaments()
+    {
+        // Arrange
+        var tournaments = GetTournaments();
+       var pagedList = new PagedList<TournamentDetail>(tournaments,tournaments.Count,pageNumber:1,pageSize:6);
+        var dtos = GetTournamentsDto();
+        _mockTournamentService.Setup(x => x.GetAllTournamentsAsync(It.IsAny<TournamentRequestParams>(), It.IsAny<bool>(), It.IsAny<bool>()))
+            .ReturnsAsync((dtos,pagedList.MetaData));
+
+        // Act
+        var tournamentsDto = await _tournamentsController.GetTournamentsAsync(new TournamentRequestParams(), sortByTitle: true);
+
+
+        // Assert
+
+        var okResult = Assert.IsType<OkObjectResult>(tournamentsDto.Result);
+        var okResultTournamentsDto = Assert.IsAssignableFrom<IEnumerable<TournamentDto>>(okResult.Value);
+        Assert.Equal(3, okResultTournamentsDto.Count());
+
+
+
+    }
+
+    private List<TournamentDto> GetTournamentsDto()
+    {
+        return new List<TournamentDto>()
+        {
+            new()
+            {
+                Id = 1,
+                Title = "Tournament1",
+                StartDate = DateTime.Now,
+                Games = []
+            },
+            new()
+            {
+                Id = 2,
+                Title = "Tournament2",
+                StartDate = DateTime.Now,
+                Games = []
+            },
+            new()
+            {
+                Id = 3,
+                Title = "Tournament3",
+                StartDate = DateTime.Now,
+                Games = []
+            }
+        };
+    }
+
+    private List<TournamentDetail> GetTournaments()
+    {
+        return new List<TournamentDetail>
+        {
+            new()
+            {
+                Id = 1,
+                Title = "Tournament1",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(3),
+                Games = []
+            },
+            new()
+            {
+                Id = 2,
+                Title = "Tournament2",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(3),
+                Games = []
+            },
+            new()
+            {
+                Id = 3,
+                Title = "Tournament3",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(3),
+                Games = []
+            },
+
+        };
+    }
+}
